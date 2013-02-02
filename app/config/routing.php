c5ee45f0ede205f1ee675b134f44285e13884d6b
varloc2000 <varloc2000@gmail.com>
2013-02-02 21:51:40 +0300

    Pass database configuration in abstract class into app/config dir and add it to gitignore

--- app/config/routing.php
+++ app/config/routing.php
@@ -1,22 +1,30 @@
 <?php
 
+namespace Configurations;
+
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Request;
 
-$routes = new Routing\RouteCollection();
+abstract class RoutingConfig
+{
+    static public function getProjectRoutes()
+    {
+        $routes = new Routing\RouteCollection();
 
-$routes->add('home', new Routing\Route('/', array(
-    '_worker' => 'Home:Home:mainPage'
-)));
+        $routes->add('home', new Routing\Route('/', array(
+            '_worker' => 'Home:Home:mainPage'
+        )));
 
-/*
- * Lessons part routes
- */
-$routes->add('lesson_list', new Routing\Route('/lessons', array(
-    '_worker' => 'Lesson:Lesson:index'
-)));
-$routes->add('lesson', new Routing\Route('/lessons/{id}', array(
-    '_worker' => 'Lesson:Lesson:lesson'
-)));
-
-return $routes;
+        /*
+         * Lessons part routes
+         */
+        $routes->add('lesson_list', new Routing\Route('/lessons', array(
+            '_worker' => 'Lesson:Lesson:index'
+        )));
+        $routes->add('lesson', new Routing\Route('/lessons/{id}', array(
+            '_worker' => 'Lesson:Lesson:lesson'
+        )));
+        
+        return $routes;
+    }
+}
--- index.php
+++ index.php
@@ -3,20 +3,32 @@
     namespace Front;
 
     require_once __DIR__ . '/autoload.php';
-    require_once 'vendor/autoload.php';
-
+    require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/app/config/routing.php';
+    require_once __DIR__ . '/app/config/database.php';
+    
+    // Symfony vendors
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing;
     
+    // Ratsreco vendors
     use Varloc\Controller\ControllerResolver;
     use Varloc\DatabaseWorker\Connector;
     
-    Connector::configure('ratsreco_storage', 'root', '80177413');
+    // Configuration
+    use Configurations\DatabaseConfig;
+    use Configurations\RoutingConfig;
+    
+    Connector::configure(
+        DatabaseConfig::getDBName(), 
+        DatabaseConfig::getDBUser(), 
+        DatabaseConfig::getDBPassword()
+    );
     
     $request = Request::createFromGlobals();
-    $routes = include __DIR__.'/app/config/routing.php';
-     
+    $routes = RoutingConfig::getProjectRoutes();
+    
     $context = new Routing\RequestContext();
     $context->fromRequest($request);
     $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
